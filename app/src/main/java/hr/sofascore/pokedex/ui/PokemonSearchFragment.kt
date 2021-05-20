e5d99c5aad71012671d8849a5d2dc18b7732abd9
Tomislav <tomislav.ukic7@gmail.com>
2021-05-20 18:36:05 +0200

    pokemon item view added

--- app/src/main/java/hr/sofascore/pokedex/ui/PokemonSearchFragment.kt
+++ app/src/main/java/hr/sofascore/pokedex/ui/PokemonSearchFragment.kt
@@ -1,15 +1,22 @@
 package hr.sofascore.pokedex.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import hr.sofascore.pokedex.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import coil.api.load
 import hr.sofascore.pokedex.databinding.FragmentPokemonSearchBinding
+import hr.sofascore.pokedex.viewmodels.PokemonViewModel
+import java.util.*
 
 
 class PokemonSearchFragment : Fragment() {
+
+    val pokemonViewModel: PokemonViewModel by activityViewModels()
 
     private var _binding: FragmentPokemonSearchBinding? = null
     private val binding get() = _binding!!
@@ -18,13 +25,29 @@ class PokemonSearchFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         super.onCreate(savedInstanceState)
 
         _binding = FragmentPokemonSearchBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        binding.searchIcon.setOnClickListener {
+            pokemonViewModel.getPokemon(
+                binding.searchPokemonTextView.text.toString()
+            )
+        }
+
+        pokemonViewModel.pokemon.observe(
+            this as LifecycleOwner,
+            {
+                binding.pokemonItem.pokemonImageView.load(pokemonImageURL(it.id))
+                binding.pokemonItem.pokemonNameTextView.text = it.name.capitalize(Locale.ROOT)
+                binding.pokemonItem.pokemonNumTextView.text = "%03d".format(it.id)
+            }
+        )
+
         return view
     }
-
 }
+
+fun pokemonImageURL(id: Int) = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"
---
+++ app/src/main/java/hr/sofascore/pokedex/ui/views/PokemonItem.kt
@@ -0,0 +1,39 @@
+package hr.sofascore.pokedex.ui.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import hr.sofascore.pokedex.R
+import hr.sofascore.pokedex.databinding.PokemonItemBinding
+
+class PokemonItem(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
+
+    private val binding: PokemonItemBinding
+
+    val pokemonNameTextView: TextView
+    val pokemonNumTextView: TextView
+    val pokemonImageView: ImageView
+    val starIconImageView: ImageView
+
+    init {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.pokemon_item, null, false)
+
+        binding = PokemonItemBinding.bind(view)
+        addView(binding.root)
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.PokemonItem,
+            0, 0
+        ).apply {
+            pokemonNameTextView = binding.pokemonNameTextView
+            pokemonNumTextView = binding.pokedexNumTextView
+            pokemonImageView = binding.pokemonImageView
+            starIconImageView = binding.startIconImageView
+        }
+    }
+}
