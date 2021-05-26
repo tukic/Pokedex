a60515c86ea06158297ab2047ee251f63c14858f
Tomislav <tomislav.ukic7@gmail.com>
2021-05-26 20:39:47 +0200

    moves table sorting implemented

--- app/src/main/java/hr/sofascore/pokedex/ui/type/Moves.kt
+++ app/src/main/java/hr/sofascore/pokedex/ui/type/Moves.kt
@@ -1,6 +1,7 @@
 package hr.sofascore.pokedex.ui.type
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,15 +11,22 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LifecycleOwner
 import hr.sofascore.pokedex.databinding.FragmentMovesBinding
+import hr.sofascore.pokedex.model.shared.PokemonMove
 import hr.sofascore.pokedex.model.shared.PokemonType
 import hr.sofascore.pokedex.viewmodels.MoveViewModel
 
 class Moves(val pokemonType: PokemonType) : Fragment() {
 
-    val moveViewModel by activityViewModels<MoveViewModel>()
+    private val moveViewModel by activityViewModels<MoveViewModel>()
 
     private var _binding: FragmentMovesBinding? = null
     private val binding get() = _binding!!
+
+    private var genAscending = true
+    private var moveAscending = true
+    private var categoryAscending = true
+    private var powerAscending = true
+    private var ppAscending = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,32 +40,108 @@ class Moves(val pokemonType: PokemonType) : Fragment() {
         moveViewModel.move.observe(
             this as LifecycleOwner,
             {
-                it.forEach {
-                    val tr = TableRow(requireContext())
-                    tr.addView(TextView(context).apply {
-                        text = it.generation.getRomanNumberGen()
-                    })
-                    tr.addView(TextView(context).apply {
-                        text = it.name
-                    })
-                    it.damage_class?.let {
-                        tr.addView(TextView(context).apply {
-                            text = it.name
-                        })
-                    }
-                    tr.addView(TextView(context).apply {
-                        text = it.power.toString()
-                    })
-                    tr.addView(TextView(context).apply {
-                        text = it.pp.toString()
-                    })
-                    binding.table.addView(tr)
-                }
+                addRowsToTable(it)
             }
         )
         moveViewModel.getMoves(pokemonType.moves.map { it.url })
 
-
+        binding.genCell.setOnClickListener {
+            if (genAscending) {
+                moveViewModel.move.value?.sortedByDescending { it.generation.name }?.let {
+                    addRowsToTable(it)
+                }
+                genAscending = false
+            } else {
+                moveViewModel.move.value?.sortedBy { it.generation.name }?.let {
+                    addRowsToTable(it)
+                }
+                genAscending = true
+            }
+        }
+        binding.moveCell.setOnClickListener {
+            if(moveAscending) {
+                moveViewModel.move.value?.sortedByDescending { it.name }?.let {
+                    addRowsToTable(it)
+                }
+                moveAscending = false
+            } else {
+                moveViewModel.move.value?.sortedBy { it.name }?.let {
+                    addRowsToTable(it)
+                }
+                moveAscending = true
+            }
+        }
+        binding.categoryCell.setOnClickListener {
+            if(categoryAscending) {
+                moveViewModel.move.value?.sortedByDescending { it.damage_class?.name }?.let {
+                    addRowsToTable(it)
+                }
+                categoryAscending = false
+            } else {
+                moveViewModel.move.value?.sortedBy { it.damage_class?.name }?.let {
+                    addRowsToTable(it)
+                }
+                categoryAscending = true
+            }
+        }
+        binding.powerCell.setOnClickListener {
+            if(powerAscending) {
+                moveViewModel.move.value?.sortedByDescending { it.power }?.let {
+                    addRowsToTable(it)
+                }
+                powerAscending = false
+            } else {
+                moveViewModel.move.value?.sortedBy { it.power }?.let {
+                    addRowsToTable(it)
+                }
+                powerAscending = true
+            }
+        }
+        binding.ppCell.setOnClickListener {
+            if(ppAscending) {
+                moveViewModel.move.value?.sortedByDescending { it.pp }?.let {
+                    addRowsToTable(it)
+                }
+                ppAscending = false
+            } else {
+                moveViewModel.move.value?.sortedBy { it.pp }?.let {
+                    addRowsToTable(it)
+                }
+                ppAscending = true
+            }
+        }
         return view
+    }
+
+    fun addRowsToTable(pokemonMove: List<PokemonMove>) {
+        while (binding.table.childCount > 1) {
+            binding.table.removeViewAt(binding.table.childCount - 1)
+        }
+        pokemonMove.forEach {
+            val row = TableRow(requireContext())
+            row.addView(TextView(context).apply {
+                text = it.generation.getRomanNumberGen()
+                gravity = Gravity.CENTER
+            })
+            row.addView(TextView(context).apply {
+                text = it.name
+                gravity = Gravity.CENTER
+            })
+            it.damage_class?.let {
+                row.addView(TextView(context).apply {
+                    text = it.name
+                    gravity = Gravity.CENTER
+                })
+            }
+            row.addView(TextView(context).apply {
+                text = it.power.toString()
+                gravity = Gravity.CENTER
+            })
+            row.addView(TextView(context).apply {
+                text = it.pp.toString()
+                gravity = Gravity.CENTER
+            })
+            binding.table.addView(row)
+        }
     }
 }
