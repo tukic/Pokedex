842d0941b105f489827742b2d073b2eec17f80bf
Tomislav <tomislav.ukic7@gmail.com>
2021-05-26 19:04:41 +0200

    move view model added

---
+++ app/src/main/java/hr/sofascore/pokedex/viewmodels/MoveViewModel.kt
@@ -0,0 +1,25 @@
+package hr.sofascore.pokedex.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import hr.sofascore.pokedex.model.networking.Network
+import hr.sofascore.pokedex.model.shared.PokemonMove
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
+
+class MoveViewModel : ViewModel() {
+    val move = MutableLiveData<List<PokemonMove>>()
+
+    fun getMoves(urls: List<String>) {
+        viewModelScope.launch {
+            val async = urls.map {
+                async {
+                    Network().getService().getPokemonMove(it).body()
+                }
+            }
+            move.value = async.awaitAll().filterNotNull()
+        }
+    }
+}
