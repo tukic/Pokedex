78b3d0051f8131839ddb7ca8b5fcc58713b65c6c
Tomislav <tomislav.ukic7@gmail.com>
2021-05-28 12:58:41 +0200

    pokeathlon stats added to pokemon activity layout

---
+++ app/src/main/java/hr/sofascore/pokedex/ui/views/PokeathlonStatsItem.kt
@@ -0,0 +1,54 @@
+package hr.sofascore.pokedex.ui.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
+import hr.sofascore.pokedex.R
+import hr.sofascore.pokedex.databinding.PokeathlonStatsItemBinding
+
+class PokeathlonStatsItem(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
+
+    private val binding: PokeathlonStatsItemBinding
+
+    init {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.pokeathlon_stats_item, null, false)
+
+        binding = PokeathlonStatsItemBinding.bind(view)
+        addView(binding.root)
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.PokeathlonStatsItem,
+            0, 0
+        ).apply {
+            binding.pokeathlonTitleTextView.text =
+                getString(R.styleable.PokeathlonStatsItem_stat_name)
+        }
+    }
+
+    fun addStars(filledStars: Int, emptyStars: Int, color: Int) {
+        binding.pokeathlonStatAmountTextView.text = "$filledStars/${filledStars + emptyStars}"
+        val starDimen = resources.getDimensionPixelSize(R.dimen.pokeathlon_star_size)
+        for (i: Int in 1..filledStars) {
+            val starView = View(context).apply {
+                background = context.getDrawable(R.drawable.ic_star_1)?.apply {
+                    setTint(context.getColor(color))
+                }
+                layoutParams = LayoutParams(starDimen, starDimen)
+            }
+            binding.pokeathlonStarsLinearLayout.addView(starView)
+        }
+        for (i: Int in 1..emptyStars) {
+            val starView = View(context).apply {
+                background = context.getDrawable(R.drawable.ic_star_0)?.apply {
+                    setTint(context.getColor(color))
+                }
+                layoutParams = LayoutParams(starDimen, starDimen)
+            }
+            binding.pokeathlonStarsLinearLayout.addView(starView)
+        }
+    }
+}
